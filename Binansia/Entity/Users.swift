88299c82b32230ef4421f336d0bd02454b1c6aad
Onur Bostan <onurbosstan@gmail.com>
2024-03-26 17:47:04 +0300

    necessary entities have been created for user information and binance information

---
+++ Binansia/Entity/Users.swift
@@ -0,0 +1,18 @@
+//
+//  Users.swift
+//  Binansia
+//
+//  Created by ONUR BOSTAN on 26.03.2024.
+//
+
+import Foundation
+
+class Users {
+    let email: String
+    let password: String
+    
+    init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+}
